01e73b5704b72fab7e93ea4367a22cd6db33db82
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2021-11-05 12:56:32 +0200

    nvim: Add scala to treesitter

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = "python",
+  ensure_installed = { "python","scala" },
   highlight = {
     enable = true, -- false will disable the whole extension
   },
