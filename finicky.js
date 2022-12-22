3225bca31d8ee46081191a581a268a2087ad4410
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2022-12-22 08:15:52 +0200

    Reformat www.office.com pattern in Finicky configuration

--- finicky.js
+++ finicky.js
@@ -14,7 +14,8 @@ module.exports = {
         match: [ 
            "https://*fortum*",
            "https://teams.microsoft.com/*",
-           "https://www.office.com", 
+           "https://www.office.com/*",
+           "https://account.activedirectory.windowsazure.com/*",
         ],
       browser: "Microsoft Edge"
     },
