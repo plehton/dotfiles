8feeb8660aefb0f3ce0831f5ec256a324fd026c6
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-02-10 15:59:34 +0200

    nvim: Add textobjects + select to Treesitter

--- nvim/lua/spec/treesitter.lua
+++ nvim/lua/spec/treesitter.lua
@@ -1,74 +1,75 @@
+local treesitter = { "nvim-treesitter/nvim-treesitter",
+build = ':TSUpdate',
+config = function()
+    require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+            "gitcommit",
+            "hcl",
+            "hocon",
+            "json",
+            "lua",
+            "python",
+            "scala",
+            "vim",
+            "vimdoc",
+            "yaml",
+        },
+        sync_install = false,
+        auto_install = true,
+
+        highlight = {
+            enable = true,
+
+            -- disable highlighting for files larger than 100kb
+            disable = function(lang, buf)
+                local max_filesize = 100 * 1024 -- 100 KB
+                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end,
+
+            additional_vim_regex_highlighting = false,
+
+        },
+
+        indent = {
+            enable = true,
+            -- disable = { "yaml" }
+        },
+
+        fold = { enable = true },
+
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
+                    ["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
+                    ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+                    ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+                    ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                },
+
+                selection_modes = {
+                    ['@parameter.outer'] = 'v',
+                    ['@function.outer'] = 'V',
+                    ['@class.outer'] = 'V',
+                },
+
+                -- Scala/Metals can't handlee whitespace, so disable it
+                include_surrounding_whitespace = false
+            },
+        },
+
+    })
+end
+    }
+
+local ts_text_objects = { "nvim-treesitter/nvim-treesitter-textobjects", dependencies = "nvim-treesitter" }
+
 return {
-    { "nvim-treesitter/nvim-treesitter",
-        name = 'treesitter',
-        build = ':TSUpdate',
-        config = function()
-
-            require("nvim-treesitter.configs").setup({
-
-                ensure_installed = {
-                    "gitcommit",
-                    "hcl",
-                    "hocon",
-                    "json",
-                    "lua",
-                    "python",
-                    "scala",
-                    "terraform",
-                    "vim",
-                    "yaml",
-                },
-                sync_install = false,
-                auto_install = false,
-                ignore_install = {},
-
-                highlight = {
-                    enable = true,
-
-                    -- disable highlighting for files larger than 100kb
-                    disable = function(lang, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            return true
-                        end
-                    end,
-
-                    additional_vim_regex_highlighting = false,
-
-                },
-
-                indent = {
-                    enable = true,
-                    -- disable = { "yaml" }
-                },
-
-                fold = { enable = true },
-
-                -- textobjects = {
-                --     move = {
-                --         enable = true,
-                --         set_jumps = true, -- whether to set jumps in the jumplist
-                --         goto_next_start = {
-                --             ["]m"] = "@function.outer",
-                --             ["]]"] = "@class.outer",
-                --         },
-                --         goto_next_end = {
-                --             ["]M"] = "@function.outer",
-                --             ["]["] = "@class.outer",
-                --         },
-                --         goto_previous_start = {
-                --             ["[m"] = "@function.outer",
-                --             ["[["] = "@class.outer",
-                --         },
-                --         goto_previous_end = {
-                --             ["[M"] = "@function.outer",
-                --             ["[]"] = "@class.outer",
-                --         },
-                --     },
-                -- },
-            })
-        end
-    },
-    -- { "nvim-treesitter/nvim-treesitter-textobjects", dependencies = "treesitter" },
+    treesitter,
+    ts_text_objects
 }
