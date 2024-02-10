ff07932cdb3e714539bfa6789a08fd04755c7f63
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-02-10 19:09:12 +0200

    nvim: Fix spec/colorscheme > all loaded at startup and use proper names

--- nvim/lua/spec/colorscheme.lua
+++ nvim/lua/spec/colorscheme.lua
@@ -3,7 +3,18 @@ return {
         lazy = false,
         priority = 1000,
     },
-    { "folke/tokyonight.nvim" },
-    { "catppuccin/nvim" },
-    { "rebelot/kanagawa.nvim" },
+    { "folke/tokyonight.nvim",
+        lazy = false,
+        -- lazy = true,
+        -- vim.cmd([[colorscheme tokyonight]])
+    },
+    { "catppuccin/nvim",
+        name = 'catppuccin',
+        lazy = false,
+        -- vim.cmd([[colorscheme catpuccin]])
+    },
+    { "rebelot/kanagawa.nvim",
+        lazy = false,
+        -- vim.cmd([[colorscheme kanagawa]])
+    },
 }
