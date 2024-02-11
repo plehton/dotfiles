b09d1babec87a449a6a24a28a0624fb983a46167
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-02-11 16:40:49 +0200

    nvim: Add Harpoon keymaps to select items from 1 to 4

--- nvim/lua/spec/harpoon.lua
+++ nvim/lua/spec/harpoon.lua
@@ -9,10 +9,10 @@ return {
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
         vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        -- vim.keymap.set("n", "<leader>a", function() harpoon:list():select(1) end)
-        -- vim.keymap.set("n", "<leader>s", function() harpoon:list():select(2) end)
-        -- vim.keymap.set("n", "<leader>d", function() harpoon:list():select(3) end)
-        -- vim.keymap.set("n", "<leader>f", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "öa", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "ös", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "öd", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "öf", function() harpoon:list():select(4) end)
         vim.keymap.set("n", "<Tab>", function() harpoon:list():next({ ui_nav_wrap = true }) end)
         vim.keymap.set("n", "<S-Tab>", function() harpoon:list():prev({ ui_nav_wrap = true }) end)
     end
