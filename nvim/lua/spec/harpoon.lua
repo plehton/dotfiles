55f594a910cd5992a5560fff47238e0040fde394
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-02-11 09:28:47 +0200

    nvim: Use harpoon2

--- nvim/lua/spec/harpoon.lua
+++ nvim/lua/spec/harpoon.lua
@@ -1,41 +1,20 @@
 return {
     'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    requires = { "nvim/lua/plenary.vim" },
     config = function()
-        require("harpoon").setup({
+        local harpoon = require("harpoon")
 
-        -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-        save_on_toggle = false,
+        harpoon:setup()
 
-        -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
-        enter_on_sendcmd = false,
-
-        -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
-        tmux_autoclose_windows = false,
-
-        -- filetypes that you want to prevent from adding to the harpoon list menu.
-        excluded_filetypes = { "harpoon" },
-
-        -- set marks specific to each git branch inside git repository
-        mark_branch = false,
-
-        -- projects = {
-        --         -- Yes $HOME works
-        --         ["$HOME/code/vim-with-me/server"] = {
-        --             term = {
-        --                 cmds = {
-        --                     "./env && npx ts-node src/index.ts"
-        --                 }
-        --             }
-        --         }
-        --     },
-
-        })
-
-        vim.keymap.set("n", "<leader>h", require('harpoon.mark').add_file)
-        vim.keymap.set("n", "<leader><Tab>", require('harpoon.ui').toggle_quick_menu)
-        vim.keymap.set("n", "<Tab>", require('harpoon.ui').nav_next)
-        vim.keymap.set("n", "<S-Tab>", require('harpoon.ui').nav_prev)
-
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        -- vim.keymap.set("n", "<leader>a", function() harpoon:list():select(1) end)
+        -- vim.keymap.set("n", "<leader>s", function() harpoon:list():select(2) end)
+        -- vim.keymap.set("n", "<leader>d", function() harpoon:list():select(3) end)
+        -- vim.keymap.set("n", "<leader>f", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<Tab>", function() harpoon:list():next({ ui_nav_wrap = true }) end)
+        vim.keymap.set("n", "<S-Tab>", function() harpoon:list():prev({ ui_nav_wrap = true }) end)
     end
 
 }
