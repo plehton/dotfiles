164a7b4f87673a6541c90e7e1f170fa4c65eee25
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-02-12 08:50:04 +0200

    nvim: Add undotree

--- nvim/lua/spec/undotree.lua
+++ nvim/lua/spec/undotree.lua
@@ -1,5 +1,16 @@
 local M = {
     'mbbill/undotree',
+    dependencies = "nvim-lua/plenary.nvim",
+    init = function ()
+        vim.g.undotree_WindowLayout = 1
+        vim.g.undotree_SplitWidth = 30
+        vim.g.undotree_ShortIndicators = 1
+        vim.g.undotree_DiffAutoOpen = 0
+        vim.g.undotree_SetFocusWhenToggle = 1
+    end,
+    keys = {
+        { "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
+    },
 }
 
 return M
