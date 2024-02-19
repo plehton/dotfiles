929766a73eaf1c55f953dbc36e09a60d952635f0
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-02-19 19:20:27 +0200

    nvim: Add treesitter-textobject/move and update keymaps

--- nvim/lua/spec/treesitter.lua
+++ nvim/lua/spec/treesitter.lua
@@ -1,71 +1,86 @@
-local treesitter = { "nvim-treesitter/nvim-treesitter",
-build = ':TSUpdate',
-config = function()
-    require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-            "gitcommit",
-            "hcl",
-            "hocon",
-            "json",
-            "lua",
-            "python",
-            "scala",
-            "vim",
-            "vimdoc",
-            "yaml",
-        },
-        sync_install = false,
-        auto_install = true,
+local treesitter = {
+    "nvim-treesitter/nvim-treesitter",
+    build = ':TSUpdate',
+    config = function()
 
-        highlight = {
-            enable = true,
-
-            -- disable highlighting for files larger than 100kb
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
-
-            additional_vim_regex_highlighting = false,
-
-        },
-
-        indent = {
-            enable = true,
-            -- disable = { "yaml" }
-        },
-
-        fold = { enable = true },
-
-        textobjects = {
-            select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                    ["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
-                    ["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
-                    ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
-                    ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
-                    ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-                },
-
-                selection_modes = {
-                    ['@parameter.outer'] = 'v',
-                    ['@function.outer'] = 'V',
-                    ['@class.outer'] = 'V',
-                },
-
-                -- Scala/Metals can't handlee whitespace, so disable it
-                include_surrounding_whitespace = false
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "gitcommit",
+                "hcl",
+                "hocon",
+                "json",
+                "lua",
+                "python",
+                "scala",
+                "vim",
+                "vimdoc",
+                "yaml",
             },
-        },
+            sync_install = false,
+            auto_install = false,
 
-    })
-end
-    }
+            highlight = {
+                enable = true,
+
+                -- disable highlighting for files larger than 100kb
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+
+                additional_vim_regex_highlighting = false,
+
+            },
+
+            indent = { enable = true, },
+
+            fold = { enable = true },
+
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+
+                        ["ab"] = { query = "@block.outer", desc = "Select outer part of a block" },
+                        ["ib"] = { query = "@block.inner", desc = "Select inner part of a block" },
+
+                        ["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
+                        ["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
+
+                        ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+                    },
+
+                    -- Scala/Metals can't handle whitespace, so disable it
+                    include_surrounding_whitespace = false
+
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]m"] = { query = "@function.outer" },
+                        ["]]"] = { query = "@class.outer" },
+                    },
+                    goto_previous_start = {
+                        ["[m"] = { query = "@function.outer" },
+                        ["[]"] = { query = "@class.outer" },
+                    },
+                    goto_next_end = {
+                        ["]M"] = { query = "@function.outer" },
+                    },
+                    goto_previous_end = {
+                        ["[M"] = { query = "@function.outer" },
+                    },
+                },
+            },
+        })
+    end
+}
 
 local ts_text_objects = { "nvim-treesitter/nvim-treesitter-textobjects", dependencies = "nvim-treesitter" }
 
