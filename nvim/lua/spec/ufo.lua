86c980c1e341583b08fe0e1ac3ff88b62fe3481d
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-02-19 20:07:06 +0200

    nvim: Change UFO provider selection from treesitter to lsp based
    Select LSP provider for filetypes given in spec. All others default to treesitter/indent. This was the other way around.
    
    Update keymaps zr and zm to their proper values zM and zR

--- nvim/lua/spec/ufo.lua
+++ nvim/lua/spec/ufo.lua
@@ -3,27 +3,28 @@ return {
     lazy = false,
     dependencies = 'kevinhwang91/promise-async',
     keys = {
-        { 'zr', function() require('ufo').openAllFolds() end },
-        { 'zm', function() require('ufo').closeAllFolds() end },
+        { 'zR', function() require('ufo').openAllFolds() end },
+        { 'zM', function() require('ufo').closeAllFolds() end },
         { 'z1', function() require('ufo').closeFoldsWith(1) end },
         { 'z2', function() require('ufo').closeFoldsWith(2) end },
         { 'z3', function() require('ufo').closeFoldsWith(3) end },
         { 'z4', function() require('ufo').closeFoldsWith(4) end },
     },
     opts = {
-            provider_selector = function(bufnr, filetype, buftype)
-            local treesitter_folding = {
-                'markdown',
-                'toml',
+        ---@diagnostic disable-next-line: unused-local
+        provider_selector = function(bufnr, filetype, buftype)
+            local lsp_folding = {
                 'json',
-                'vim',
-                'vimdoc',
+                'lua',
+                'python',
+                'scala',
+                'terraform',
             }
-                if vim.tbl_contains(treesitter_folding, filetype) then
-                    return { 'treesitter', 'indent' }
-                end
+            if vim.tbl_contains(lsp_folding, filetype) then
                 return { "lsp", "indent" }
             end
+            return { 'treesitter', 'indent' }
+        end
 
     },
 
