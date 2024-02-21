5bb3715444bfa87779f4e38d72ea8661cc45a56d
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-02-21 20:31:07 +0200

    nvim: Add lsp.buf.format mappings, require folke/neodev before lsp-config

--- nvim/lua/spec/lsp.lua
+++ nvim/lua/spec/lsp.lua
@@ -1,51 +1,61 @@
-local lsp_keymaps = function(bufnr)
-
+local on_attach = function(client, bufnr)
     local opts = { buffer = bufnr, noremap = true, silent = true }
 
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gd',vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gr',vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)
 
     vim.keymap.set('n', '[e', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', ']e', vim.diagnostic.goto_next, opts)
     vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-    vim.keymap.set('n', '<leader>eq',vim.diagnostic.setqflist, opts)
-    vim.keymap.set('n', '<leader>el',vim.diagnostic.setloclist, opts)
+    vim.keymap.set('n', '<leader>eq', vim.diagnostic.setqflist, opts)
+    vim.keymap.set('n', '<leader>el', vim.diagnostic.setloclist, opts)
 
-end
-
-local on_attach = function(_, bufnr)
-    lsp_keymaps(bufnr)
+    vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)
+    vim.keymap.set('v', '<leader>f', vim.lsp.buf.format, opts)
 end
 
 local common_capabilities = function()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+    capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {
+            "documentation",
+            "detail",
+            "additionalTextEdits",
+        },
+    }
+
+    -- required by UFO
     capabilities.textDocument.foldingRange = {
         dynamicRegistration = false,
         lineFoldingOnly = true
     }
+
     return capabilities
 end
 
+-- LSP configuration
+--
 local lsp = {
     'neovim/nvim-lspconfig',
-    -- event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
         'hrsh7th/nvim-cmp',
-        'hrsh7th/cmp-nvim-lsp',
-        'folke/neodev.nvim',
     },
+    opts = { inlay_hints = { enabled = true } }
 }
 
 lsp.config = function()
+    require('neodev').setup()
+
     local lspconfig = require('lspconfig')
     local icons = require('pjl.icons')
 
     local servers = {
+        'jsonls',
         'lua_ls',
         'pyright',
         'terraformls',
@@ -91,28 +101,27 @@ lsp.config = function()
             opts = vim.tbl_deep_extend("force", settings, opts)
         end
 
-        if server == "lua_ls" then
-            require("neodev").setup {}
-        end
-
         lspconfig[server].setup(opts)
     end
-
 end
 
+
+-- Scala/Metals configuration
+--
 local metals = {
     "scalameta/nvim-metals",
     dependencies = {
-      "nvim-lua/plenary.nvim",
+        "nvim-lua/plenary.nvim",
     },
-    -- ft = { "scala", "sbt", "java", "sc" },
 }
 
 metals.opts = function()
-
     local metals_config = require("metals").bare_config()
 
     metals_config = {
+        init_options = {
+            statusBarProvider = "off"
+        },
         settings = {
             showImplicitArguments = true,
             excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
@@ -120,15 +129,19 @@ metals.opts = function()
         capabilities = common_capabilities(),
         on_attach = on_attach
     }
+
     return metals_config
 end
 
-metals.config = function(_, metals_config)
+---@diagnostic disable-next-line: unused-local
+metals.config = function(self, metals_config)
     local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+
     vim.api.nvim_create_autocmd("FileType", {
         pattern = { "scala", "sbt", "java", "sc" },
         callback = function()
             require("metals").initialize_or_attach(metals_config)
+
             vim.keymap.set('n', '<leader>sc', function()
                 require('metals').hover_worksheet()
             end)
