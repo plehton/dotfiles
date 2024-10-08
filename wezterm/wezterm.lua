6d4a3d38ddc694aff0c818a077df7dcce1f43500
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-10-08 10:51:25 +0300

    wezterm: Update font, remove keymaps, default scheme set in .colorscheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,35 +1,22 @@
 local wezterm = require 'wezterm'
 local config = require 'wezterm'.config_builder()
 
-local DEFAULT_COLORSCHEME = "rose-pine"
-
-
--- local gpus = wezterm.gui.enumerate_gpus()
--- config.webgpu_preferred_adapter = gpus[1]
--- config.front_end = "WebGpu"
-
-config.font = wezterm.font('JetBrains Mono', { weight = 'Light' })
+config.font = wezterm.font('JetBrains Mono', { weight = 'Regular' })
 config.font_size = 15
 
-config.enable_tab_bar = true
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
 
 config.window_padding = {
-    left = 2,
-    right = 2,
-    top = 2,
-    bottom = 2,
+    left = 1,
+    right = 1,
+    top = 1,
+    bottom = 1,
 }
 
-config.keys = {
-    -- We don't want to use tabs while we are in tmux world
-    { key = 'T', mods = 'CTRL',       action = wezterm.action.DisableDefaultAssignment },
-    { key = 'T', mods = 'SHIFT|CTRL', action = wezterm.action.DisableDefaultAssignment },
-    { key = 't', mods = 'SHIFT|CTRL', action = wezterm.action.DisableDefaultAssignment },
-    { key = 't', mods = 'SUPER',      action = wezterm.action.DisableDefaultAssignment },
-}
+config.keys = {}
 
 -- Colorscheme
---
 local colorscheme_file = os.getenv("HOME") .. "/.colorscheme"
 wezterm.add_to_config_reload_watch_list(colorscheme_file)
 
@@ -39,8 +26,7 @@ local file = io.open(colorscheme_file, "r")
 if file then
     local scheme = file:read("*l")
     scheme = colorschemes[scheme]
-    config.color_scheme = scheme ~= nil and scheme or DEFAULT_COLORSCHEME
-    wezterm.log_info(config.color_scheme)
+    config.color_scheme = scheme
     file:close()
 end
 
