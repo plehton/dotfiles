40acce2a111e008ceb5712cbb2d2709991d2522c
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-10-08 10:50:54 +0300

    wezterm: Add tokyo night + rose pine dawn to colorschemes

--- wezterm/colorschemes.lua
+++ wezterm/colorschemes.lua
@@ -1,10 +1,13 @@
 return {
     ["tokyonight-day"]       = "Tokyo Night Day",
+    ["tokyonight-moon"]      = "Tokyo Night Moon",
+    ["tokyonight-night"]     = "Tokyo Night",
     ["tokyonight-storm"]     = "Tokyo Night Storm",
     ["catppuccin-frappe"]    = "Catppuccin Frappe",
     ["catppuccin-latte"]     = "Catppuccin Latte",
     ["catppuccin-mocha"]     = "Catppuccin Mocha",
     ["catppuccin-macchiato"] = "Catppuccin Macchiato",
+    ["rose-pine-dawn"]       = "rose-pine-dawn",
     ["rose-pine-main"]       = "rose-pine",
     ["rose-pine-moon"]       = "rose-pine-moon",
 }
