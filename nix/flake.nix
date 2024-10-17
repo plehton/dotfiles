14d87922988448dc06d8f8c3936c466bad58bbe3
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-10-17 17:23:11 +0300

    nix: Refactor flake
    add shell programs, setup zsh

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,57 +1,120 @@
 {
-  description = "MacOS Darwin setup";
+  description = "Petri's MacOS Darwin setup";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = [ ];
+  outputs = inputs@{ self, nixpkgs, darwin, home-manager, ... }: {
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+    darwinConfigurations.MV9J7YK4N9 = darwin.lib.darwinSystem {
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+      modules = [
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
+        # Darwin
+        ({ pkgs, ... }: {
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+          nixpkgs.hostPlatform = "aarch64-darwin";
+          services.nix-daemon.enable = true;
+          system.stateVersion = 5;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+          nix.extraOptions = ''
+              experimental-features = nix-command flakes
+          '';
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+          # Shell
+          environment.loginShell = pkgs.zsh;
+          environment.shells = [ pkgs.bash pkgs.zsh ];
+          programs.zsh.enable = true;  # default shell on catalina
 
-      # MacOS Settings
-      security.pam.enableSudoTouchIdAuth = true;
-      system.defaults = {
-          dock.autohide = true;
-          dock.mru-spaces = false;
-          screensaver.askForPasswordDelay = 60;
-      };
+          # System level packages for all users
+          environment.systemPackages = with pkgs; [
+            coreutils
+            lua
+            nixfmt-rfc-style
+          ];
 
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
+          users.users.pjl.home = "/Users/pjl";
 
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."simple".pkgs;
+          fonts.packages = [  (pkgs.nerdfonts.override { fonts = ["JetBrainsMono" "FiraCode"]; }) ];
+
+          # MacOS Settings
+          security.pam.enableSudoTouchIdAuth = true;
+          system.defaults = {
+              NSGlobalDomain.AppleShowAllExtensions = true;
+              NSGlobalDomain.InitialKeyRepeat = 14;
+              NSGlobalDomain.KeyRepeat = 1;
+              dock.autohide = true;
+              dock.mru-spaces = false;
+              finder.AppleShowAllExtensions = true;
+              finder._FXShowPosixPathInTitle = true;
+              screensaver.askForPasswordDelay = 60;
+          };
+
+        }) # darwin module
+
+        # Home manager
+        home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+          };
+
+          home-manager.users.pjl = { pkgs, ...} : {
+
+            home.stateVersion = "24.11";
+
+            home.packages = with pkgs; [
+              curl
+              fd
+              git
+              neovim
+              ripgrep
+              wget
+            ];
+
+
+            programs = {
+
+              # alacritty = {
+              #   enable = false;
+              #   settings.font.normal.family = "Jetbrains Mono";
+              #   settings.fong.size = 14;
+              # };
+              bat.enable = true;
+              fzf = {
+                enable = true;
+                enableZshIntegration = true;
+                defaultCommand = "fd --type f --follow --hidden --exclude .git";
+              };
+              lazygit.enable = true;
+              z-lua = {
+                enable = true;
+                enableZshIntegration = true;
+                enableAliases = false;
+                options = [ "fzf" "once" ];
+              };
+              zsh = {
+                enable = true;
+                autocd = true;
+                autosuggestion.enable = true;
+                enableCompletion = true;
+                historySubstringSearch.enable = true;
+                syntaxHighlighting.enable = true;
+                initExtra = "source $HOME/code/dotfiles/zshrc";
+              };
+
+            }; # programs
+          }; # users.pjl
+        } # home-manager module
+      ]; # darwin modules
+    }; # darwinSystem
+    # darwinPackages = self.darwinConfigurations.MV9J7YK4N9.pkgs;
   };
 }
+
