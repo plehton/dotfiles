edcd96e2004049d2b36620aa881d372b083789ea
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-11-02 07:32:21 +0200

    nvim: Add nvimtree (again)

---
+++ nvim/lua/spec/nvimtree.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config =  function()
+      require("nvim-tree").setup()
+    end,
+    keys = {
+      {
+        '<C-P>',
+        function()
+          require("nvim-tree.api").tree.open({ find_file = true })
+        end,
+        desc = 'Find file in Tree'
+      },
+    }
+  },
+};
