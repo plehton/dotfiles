b8dcc52b990ab11fbedadc161f81640fcd7ca73a
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2025-03-18 20:42:02 +0200

    wezterm: autoswitch colorscheme using theme files from colors directory

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,40 +1,22 @@
 local wezterm = require 'wezterm'
 local config = require 'wezterm'.config_builder()
 
--- Set up dynamic switching of colorscheme:
+-- Set up dynamic switching of colorscheme
 --
--- ~/.colorscheme contains the name of the colorscheme which is shared
--- with other apps, especially neovim. Alacritty and Kitty can be easily
--- configured to share the same colorscheme names. Since Wezterm has it's
--- own colorsceme database, we need to create a mapping for the names.
-
+-- Poll ~/.colorscheme for changes and read the name of
+-- the colorscheme from the file.
+--
 local colorscheme_file = os.getenv("HOME") .. "/.colorscheme"
 wezterm.add_to_config_reload_watch_list(colorscheme_file)
-
-local COLORSCHEMES = {
-    ["catppuccin-frappe"]    = "Catppuccin Frappe",
-    ["catppuccin-latte"]     = "Catppuccin Latte",
-    ["catppuccin-macchiato"] = "Catppuccin Macchiato",
-    ["catppuccin-mocha"]     = "Catppuccin Mocha",
-    ["kanagawa-dragon"]      = "Kanagawa Dragon",
-    ["kanagawa-lotus"]       = "Kanagawa Lotus",
-    ["kanagawa-wave"]        = "Kanagawa Wave",
-    ["rose-pine-dawn"]       = "rose-pine-dawn",
-    ["rose-pine-main"]       = "rose-pine",
-    ["rose-pine-moon"]       = "rose-pine-moon",
-    ["tokyonight-day"]       = "Tokyo Night Day",
-    ["tokyonight-moon"]      = "Tokyo Night Moon",
-    ["tokyonight-night"]     = "Tokyo Night",
-    ["tokyonight-storm"]     = "Tokyo Night Storm",
-}
 
 local get_colorscheme_name = function(cf)
     local file = io.open(cf, "r")
     if file then
         local scheme = file:read("*l")
-        scheme = COLORSCHEMES[scheme]
         file:close()
+        return scheme
     end
+    return nil
 end
 
 --
@@ -46,11 +28,13 @@ config = {
 
     font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'ExtraLight' }),
     font_size = 16,
+    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 
     window_decorations = "RESIZE",
     enable_tab_bar = false,
     adjust_window_size_when_changing_font_size = false,
     color_scheme = get_colorscheme_name(colorscheme_file),
+    -- color_scheme = "Forestbones_dark",
     window_padding = {
         left = 10,
         right = 10,
