80d594e97196cbc92a497d7563900c5a07e62d03
Petri Lehtonen <petri.lehtonen@fortum.com>
2025-10-12 09:41:19 +0300

    nvim: Lua ls works with VIMRUNTIME

--- nvim/.config/nvim/lsp/lua_ls.lua
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -1,19 +1,27 @@
 return {
-    cmd = { 'lua-language-server' },
-    filetypes = { 'lua' },
-    root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-    settings = {
-        Lua = {
-            runtime = { version = 'LuaJIT' },
-            format = {
-                enable = true
-            },
-            workspace = {
-                checkThirdParty = false,
-            },
-            diagnostics = {
-                disable = { "missing-parameter", "missing-fields" }
-            },
-        },
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+  settings = {
+    Lua = {
+      runtime = { version = 'LuaJIT' },
+      format = {
+        enable = true,
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          vim.fn.stdpath("config"),
+          vim.fn.stdpath("data") .. "/lazy/",
+          "${3rd}/luv/library"
+        }
+
+      },
+      diagnostics = {
+        globals = { "vim" },
+        disable = { "missing-parameter", "missing-fields" }
+      },
     },
+  },
 }
