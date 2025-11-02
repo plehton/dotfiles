ea38b97bdb33b5df9912d0f8f8c64156f82e9412
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2025-11-02 20:52:50 +0200

    zsh: Add tmux-session-chooser

---
+++ zsh/.zsh/bin/tmux-session-chooser.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get a list of tmux sessions
+sessions=$(tmux list-sessions -F "#{session_name}")
+
+# Use fzf to select a session
+selected_session=$(echo "$sessions" | fzf --tmux center \
+    --reverse \
+    --style full \
+    --border \
+    --padding 1,1 \
+    --border-label ' Choose session ' \
+    --input-label  '')
+
+# If a session was selected, attach to it
+if [ -n "$selected_session" ]; then
+    tmux switch-client -t "$selected_session"
+fi
