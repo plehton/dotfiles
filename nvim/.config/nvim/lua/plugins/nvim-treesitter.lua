0679c862067411ff14a43a2e301cf05e3569ab10
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2026-05-27 12:43:54 +0300

    nvim: Fix treesitter plugin spec
    remove help and use build=':TSUpdate'

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,9 +3,9 @@ return {
         "nvim-treesitter/nvim-treesitter",
         branch = "main",
         lazy = false,
-        build = "TSUpdate",
+        build = ":TSUpdate",
         config = function()
-            local types = { 'bash', 'python', 'vim', 'help', 'lua', 'scala', 'terraform' }
+            local types = { 'bash', 'python', 'vim', 'lua', 'scala', 'terraform' }
             require 'nvim-treesitter'.install(types)
             local augroup = vim.api.nvim_create_augroup("pjl-treesitter", { clear = true })
             vim.api.nvim_create_autocmd('FileType', {
@@ -14,6 +14,7 @@ return {
                 callback = function()
                     vim.treesitter.start()
                     vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                    vim.wo.foldmethod = 'expr'
                     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
                 end,
             })
