f5b77fecea9b104cca46bc7bef3e620fa2c393ea
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2026-06-08 15:59:12 +0300

    nvim: refine lsp.folding definition

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -14,7 +14,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
         if client:supports_method('textDocument/foldingRange') then
             local win = vim.api.nvim_get_current_win()
-            vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+            vim.wo[win].foldmethod = 'expr'
+            vim.wo[win].foldexpr = 'v:lua.vim.lsp.foldexpr()'
         end
 
         if not client:supports_method('textDocument/willSaveWaitUntil')
