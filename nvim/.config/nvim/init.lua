ad54fcc415feb03b81dd10114014a11d5412c234
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2026-06-22 13:36:50 +0300

    nvim: cleanse plugins in init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,34 +6,32 @@ require 'config.lsp'
 require 'config.autocommands'
 
 vim.pack.add({
-    -- Colorschemes
-    "https://github.com/catppuccin/nvim",
-    "https://github.com/p00f/alabaster.nvim",
-    "https://github.com/marekh19/meowsoot.nvim",
-    "https://github.com/rose-pine/neovim",
     -- Core
     "https://github.com/tpope/vim-eunuch",
     "https://github.com/tpope/vim-repeat",
     "https://github.com/tpope/vim-surround",
     "https://github.com/tpope/vim-unimpaired",
-    "https://github.com/nvim-reesitter/nvim-treesitter",
+    "https://github.com/tpope/vim-fugitive",
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
     -- "https://github.com/ibhagwan/fzf-lua",
     "https://github.com/nvim-telescope/telescope.nvim",
     "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
     "https://github.com/nvim-lua/plenary.nvim",
     -- File manager
     "https://github.com/justinmk/vim-dirvish",
-    -- Git
-    "https://github.com/tpope/vim-fugitive",
     -- UI
     "https://github.com/lukas-reineke/indent-blankline.nvim",
     -- LSP
     "https://github.com/scalameta/nvim-metals",
+    -- "https://github.com/artemave/workspace-diagnostics.nvim",
     -- AI
     "https://github.com/zbirenbaum/copilot.lua",
     "https://github.com/CopilotC-Nvim/CopilotChat.nvim",
-    -- Disabled
-    -- "https://github.com/artemave/workspace-diagnostics.nvim",
+    -- Colorschemes
+    "https://github.com/catppuccin/nvim",
+    "https://github.com/p00f/alabaster.nvim",
+    "https://github.com/marekh19/meowsoot.nvim",
+    "https://github.com/rose-pine/neovim",
 })
 
 -- Synchronous build for telescope-fzf-native (PackChanged doesn't fire during initial add)
@@ -53,4 +51,3 @@ vim.api.nvim_create_autocmd('PackChanged', {
 })
 
 vim.cmd.colorscheme("catppuccin")
-
