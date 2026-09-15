b35a138be3b0821a565b698dfce3517033933902
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2026-09-15 10:21:17 +0300

    finicky: Update config to latest version v4

--- finicky/.finicky.js
+++ finicky/.finicky.js
@@ -1,37 +1,44 @@
 // ~/.finicky.js
-module.exports = {
+export default {
   defaultBrowser: "Safari",
   rewrite: [
     {
       // Redirect all urls to use https
-      match: ({ url }) => url.protocol === "http",
-      url: { protocol: "https" },
+      match: (url) => url.protocol === "http:",
+      url: (url) => {
+        url.protocol = "https:";
+        return url;
+      },
     },
     {
       // Redirect all localhost urls use http
-        match: ({ url }) => url.host === "localhost",
-      url: { protocol: "http" },
+      match: (url) => url.hostname === "localhost",
+      url: (url) => {
+        url.protocol = "http:";
+        return url;
+      },
     },
     {
       // use old.reddit.com
-      match: ({ url }) => url.host.endsWith("reddit.com"),
-      url: ({url}) => ({ "host": "old.reddit.com" }),
+      match: (url) => url.hostname.endsWith("reddit.com"),
+      url: (url) => {
+        url.hostname = "old.reddit.com";
+        return url;
+      },
     },
   ],
   handlers: [
     {
       match: [
-          "https://webars.*.com/myitportal/*",
-          "https://fortum.service-now.com/*",
-          "https://arsazureforms.fortum.com/*",
+        "https://webars.*.com/myitportal/*",
+        "https://fortum.service-now.com/*",
+        "https://arsazureforms.fortum.com/*",
       ],
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
-      match: [
-          "https://*youtube*",
-      ],
-      browser: "Brave Browser"
+      match: ["https://*youtube*"],
+      browser: "Brave Browser",
     },
-  ]
+  ],
 };
